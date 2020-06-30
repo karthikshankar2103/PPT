bae7dba15e77bcf76e271ab8ab8984b71d7bda2d
karthikshankar2103 <62713456+karthikshankar2103@users.noreply.github.com>
2020-06-30 19:18:39 +0530

    Update PPT.java

--- src/main/java/demo/PPT.java
+++ src/main/java/demo/PPT.java
@@ -2,7 +2,7 @@ package demo;
 
 public class PPT {
 	public static void main(String[] args) {
-		String sk = "Karthi shankar";
+		String sk = "Karthik";
 		String s = "Karthik";
 		String k = "Shankar";
 	}
